93d83730b31dfd28dac85ef445c1fcb673857cc1
Álvaro Guzmán López <guzmanlopezalvaro@gmail.com>
2025-12-07 20:28:43 -0300

    Hito2 - Creación de Formularios Register y Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 import Navbar from './components/navBar/navBar.jsx'
 import Home from './components/home/home.jsx'
+import RegisterPage from './components/RegisterPage/RegisterPage.jsx'
+import LoginPage from './components/LoginPage/LoginPage.jsx'
 import Footer from './components/footer/footer.jsx'
+
+
 
 function App() {
   
@@ -9,7 +13,9 @@ function App() {
   return (
     <>
       <Navbar></Navbar> 
-      <Home></Home>
+      {/*<Home></Home>*/}
+      {/*<RegisterPage></RegisterPage>*/}
+      <LoginPage></LoginPage>
       <Footer></Footer> 
     </>
   )
